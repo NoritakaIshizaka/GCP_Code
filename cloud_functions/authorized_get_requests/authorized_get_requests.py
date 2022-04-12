d80fa1f267c918dcd8edeaa53ebfb6a3bd8961b1
NoritakaIshizaka <nishizaka1015@gmail.com>
2022-04-12 23:10:00 +0900

    add authorized_get_request

---
+++ cloud_functions/authorized_get_requests/authorized_get_requests.py
@@ -0,0 +1,33 @@
+import requests
+
+import google.auth.transport.requests
+import google.oauth2.id_token
+
+def authorized_get_request(target_url, params):
+    '''
+    Description
+        Cloud Functions(HTTPトリガー)に対して、起動元の認証を実装
+    Args
+        target_url:Cloud FunctionのトリガーURL
+        params:HTTP(GET)のパラメータ
+    '''
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, target_url)
+    headers = {
+        "Authorization": f"Bearer {id_token}"
+    }
+    
+    response = requests.get(target_url,headers=headers, params=params)
+
+    return response
+
+
+if __name__ == "__main__":
+    target_url ="WRITE Cloud Functions Trigger URL"
+    result = authorized_get_request(
+        target_url = target_url,
+        params = {
+            "message":"sample message"
+        }
+    )
+    print(result.text)
